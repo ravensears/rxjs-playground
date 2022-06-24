9ed98913b12fdd7434b61d0f598774bebd7d5864
Raven <raven.sears@bcbgroup.io>
2022-06-24 17:12:03 +0100

    Unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,40 @@
 import { Observable } from "rxjs";
 
-const observable = new Observable<string>((subscriber) => {
-  subscriber.next("Alice");
-  subscriber.next("Ben");
-  setTimeout(() => {
-    subscriber.next("Charlie");
-  }, 2000);
-  // setTimeout(() => {
-  //   subscriber.error(new Error("Failure"));
-  // }, 4000);
+const interval = new Observable<number>((subscriber) => {
+  let counter = 1;
+
+  const intervalId = setInterval(() => {
+    console.log("Emitted", counter);
+    subscriber.next(counter++);
+  }, 1000);
+
   return () => {
-    console.log("Teardown");
+    clearInterval(intervalId);
   };
 });
 
-observable.subscribe({
-  next: (value) => console.log(value),
-  // error: (err) => console.log(err.message),
-  complete: () => console.log("Completed"),
-});
+const subscription = interval.subscribe((value) => console.log(value));
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 10000);
+
+// const observable = new Observable<string>((subscriber) => {
+//   subscriber.next("Alice");
+//   subscriber.next("Ben");
+//   setTimeout(() => {
+//     subscriber.next("Charlie");
+//   }, 2000);
+//   setTimeout(() => {
+//     subscriber.error(new Error("Failure"));
+//   }, 4000);
+//   return () => {
+//     console.log("Teardown");
+//   };
+// });
+
+// observable.subscribe({
+//   next: (value) => console.log(value),
+//   error: (err) => console.log(err.message),
+//   complete: () => console.log("Completed"),
+// });

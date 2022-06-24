d4a6ca97c0fd541231422496e5159b80ffc56b23
Raven <raven.sears@bcbgroup.io>
2022-06-24 16:23:05 +0100

    Next Notifications

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,9 @@
 import { Observable } from "rxjs";
 
 const observable = new Observable<string>((subscriber) => {
-  console.log("Oberservable executed");
   subscriber.next("Alice");
-  setTimeout(() => subscriber.next("Ben"), 2000);
-  setTimeout(() => subscriber.next("Charlie"), 4000);
+  subscriber.next("Ben");
+  setTimeout(() => subscriber.next("Charlie"), 3000);
 });
 
-console.log("Subscription 1 starts");
-observable.subscribe((value: string) => console.log("Subscription 1:", value));
-
-setTimeout(() => {
-  console.log("Subscription 2 starts");
-  observable.subscribe((value: string) => console.log("Subscription 2:", value));
-}, 1000);
-
-// const subscription = observable.subscribe((value: string) =>
-//   console.log(value)
-// ); 
-
-// setTimeout(() => {
-//   console.log("Unsubscribe");
-//   subscription.unsubscribe();
-// }, 3000);
+observable.subscribe((value) => console.log(value));

c3d6a15236a31b250f3a84fdd426403f2674a830
Raven <raven.sears@bcbgroup.io>
2022-06-24 16:41:56 +0100

    Error and Completed Notifications

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,19 @@ import { Observable } from "rxjs";
 const observable = new Observable<string>((subscriber) => {
   subscriber.next("Alice");
   subscriber.next("Ben");
-  setTimeout(() => subscriber.next("Charlie"), 3000);
+  setTimeout(() => {
+    subscriber.next("Charlie");
+  }, 2000);
+  // setTimeout(() => {
+  //   subscriber.error(new Error("Failure"));
+  // }, 4000);
+  return () => {
+    console.log("Teardown");
+  };
 });
 
-observable.subscribe((value) => console.log(value));
+observable.subscribe({
+  next: (value) => console.log(value),
+  // error: (err) => console.log(err.message),
+  complete: () => console.log("Completed"),
+});
